6b055d75ed39db2d8f726df401be55d9d01f6cde
RobMarv1n <fenalfan@gmail.com>
2021-09-11 01:01:02 +0300

    global update

--- samogon/assets/jquery/script.js
+++ samogon/assets/jquery/script.js
@@ -1,31 +1,4 @@
-// ! HEADER SLIDER
-
-const swiper = new Swiper('.popular-products__slider', {
-
-    slidesPerView: 3,
-    spaceBetween: 25,
-    simulateTouch: false,
-    // grabCursor: true,
-    loop: true,
-    
-    // autoplay: 4000,
-    // speed: 400,
-    nextButton: '.popular-products__slider-button--button-next',
-    prevButton: '.popular-products__slider-button--button-prev',
-});
-
-const swiper2 = new Swiper('.discount-products__slider', {
-
-    slidesPerView: 3,
-    spaceBetween: 25,
-    simulateTouch: false,
-    // grabCursor: true,
-    loop: true,
-    // autoplay: 4000,
-    // speed: 400,
-    nextButton: '.discount-products__slider-button--button-next',
-    prevButton: '.discount-products__slider-button--button-prev',
-});
+// ! MAIN HEADER SEARCH
 
 jQuery(function () {
     var availableTags = [
@@ -50,3 +23,151 @@ jQuery(".main-header__search-input").autocomplete({
 
 });
 
+//	!ANCHOR HEADER NAV
+
+jQuery(document).ready( function () {
+  
+    jQuery("a.scroll-to").on("click", function(e){
+          e.preventDefault();
+          var anchor = $(this).attr('href');
+          jQuery('html, body').stop().animate({
+              scrollTop: $(anchor).offset().top
+          }, 800);
+      });
+    
+  });
+
+// ! MAIN PAGE PRODUCTS SLIDERS
+
+const swiper = new Swiper('.popular-products__slider', {
+
+    slidesPerView: 3,
+    spaceBetween: 25,
+    simulateTouch: false,
+    loop: true,
+    nextButton: '.popular-products__slider-button--button-next',
+    prevButton: '.popular-products__slider-button--button-prev',
+
+});
+
+const swiper2 = new Swiper('.discount-products__slider', {
+
+    slidesPerView: 3,
+    spaceBetween: 25,
+    simulateTouch: false,
+    loop: true,
+    nextButton: '.discount-products__slider-button--button-next',
+    prevButton: '.discount-products__slider-button--button-prev',
+
+});
+
+// !CATALOG SLIDERS
+
+jQuery(document).ready(function(){
+    const catalogSlider = new Swiper('.products__slider', {
+        spaceBetween: 25,
+        simulateTouch: false,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        slidesPerColumnFill: 'row',
+        speed: 0,
+
+        grid: {
+            rows: 4, 
+            fill: 'row',
+        },
+
+        pagination: {
+            el: '.products__slider-pagination',
+            clickable: true,
+            renderBullet: function (index, className) {
+              return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
+        
+    });
+});
+
+// ! LOAD PAGES
+
+// jQuery(document).ready(function($){
+//     jQuery(document).on('click', '.calculator__link', function(event){
+//         event.preventDefault();
+//         var linkCat = jQuery(this).attr('href');
+//         jQuery.ajax({
+//         type: 'post',
+//         url: url, 
+//         data: {
+//         action: 'my_aj',
+//         'id':linkCat,
+//         },
+//         success: function(linkCat){
+//         $('.products').load(linkCat); 
+//         },
+//         })
+//         });
+//     });
+
+// jQuery(document).ready(function() {
+//     jQuery(document).on('click', '.calculator__link', function(event) {
+//         event.preventDefault();
+//         jQuery('.products').empty().load('calculator.html');
+//     });
+// });
+
+//   ! CONTACTS REQUIRED FORM
+
+jQuery('document').ready(function() {
+    jQuery('.contacts__button').on('click', function() {
+        jQuery('input[required]').addClass('required');
+        jQuery('textarea[required]').addClass('required');
+    });
+  });
+
+// ! CONTACTS MAP
+
+// function myMap() {
+//     var mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.12),
+//         zoom: 10,
+//         mapTypeId: google.maps.MapTypeId.HYBRID
+//     }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// }
+
+// ! CALCULATOR
+
+jQuery('document').ready(function() {
+
+    
+
+    jQuery(".calculator__form").on("change", '.calculator__input', function() {
+
+        let value = jQuery('#volume').val();
+        let percentageUp = jQuery('#percentage-up-to').val();
+        let percentageAfter = jQuery('#percentage-after').val();
+
+        if (percentageUp < percentageAfter) {
+            jQuery('.calculator__help').html("Процент до разведения должен быть больше процента после");
+        }
+
+        else if (allFilled()) {
+            
+            let answer = ((100 * percentageUp * (value * 1000 / 100)) / percentageAfter - 100 * (value * 1000 / 100))/1000;
+    
+            jQuery('.calculator__help').html("Для получения <span>" + percentageAfter + "%</span> после разбавления, нужно добавить <span>" + answer + " литров воды.</span>");
+            jQuery('.calculator__img').addClass('decided') 
+        }
+        
+        function allFilled() {
+            var filled = true;
+            jQuery('#volume, #percentage-up-to, #percentage-after').each(function() {
+                if(jQuery(this).val() == '') filled = false;
+            });
+            return filled;
+        }
+
+    });
+
+
+});
